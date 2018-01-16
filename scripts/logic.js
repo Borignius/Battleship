619f194468b3ec413bc0d831d57067cb1168366e
Job <jobsmith9@gmail.com>
2018-01-16 14:56:16 -0700

    finished product

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,14 +1,40 @@
 function fireMissle(displayBoard, board, coordinates) {
-  return displayBoard
+  var missleloaction = board [coordinates.y] [coordinates.x];
+  if ( missleloaction > 1) {
+   displayBoard[coordinates.y] [coordinates.x] = hit;
+ } else {
+   displayBoard[coordinates.y] [coordinates.x] = miss;
+ }
+  // console.log (displayBoard);
+  return displayBoard;
 }
 
 function generateComputerMove(board) {
-  return {
-    x: 0,
-    y: 0
+  var compy = (Math.floor(Math.random() * 10));
+  var compx = (Math.floor(Math.random() * 10));
+  board.x = compx;
+  board.y = compy;
+  if (board [compy] [compx] == -1 || board [compy] [compx] == 1 ){
+    generateComputerMove(board);
   }
+  return {x: board.x, y: board.y}
 }
 
-function isWon(board) {
-  return false
+function isWon(board){
+  var totes = 0;
+  var windscar = 17;
+  board.forEach(function(row){
+    row.forEach(function(box){
+      if (box == h) {
+        totes++
+      }
+    })
+  })
+  if (totes === windscar){
+    gameOver = true;
+  } else {
+    gameOver = false;
+  }
+  console.log(totes)
+  return gameOver;
 }
